ea75f7ab394b52974114cb0ce9291aebebd6fb2a
Sumit Subhash Patil <sumitgatpatil41@gmail.com>
2025-08-31 20:41:00 +0530

    added vercel.json file to front end to handel routing

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'], // Frontend URLs
+    origin: ['https://note-taker-virid.vercel.app'], // Frontend URLs
     credentials: true
 }));
 app.use(express.json());
